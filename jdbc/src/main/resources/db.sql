b2a91035dd2cabf2715d6617af18063da811514d
Eugene <jinstrame@gmail.com>
2016-12-05 06:15:10 +0300

    updated db script

--- jdbc/src/main/resources/db.sql
+++ jdbc/src/main/resources/db.sql
@@ -1,4 +1,5 @@
 Drop user tomcat;
+
 CREATE USER tomcat with ENCRYPTED PASSWORD 'password';
 
 CREATE SCHEMA web_app;
@@ -13,14 +14,15 @@ CREATE TABLE web_app.pages(
   first_name VARCHAR(100) NOT NULL ,
   second_name VARCHAR(30) NOT NULL ,
   dob DATE,
+  timezone VARCHAR(20),
   language VARCHAR(7),
-  main_post_id INT,
-  last_post_id INT
+  last_post_id INT,
+  about VARCHAR(4049)
 );
 
 CREATE TABLE web_app.accounts(
   login VARCHAR(30) PRIMARY KEY ,
-  h_password VARCHAR(60),
+  h_password VARCHAR(100),
   token INT,
   person_id INT REFERENCES web_app.pages(id) ON DELETE CASCADE
 );
@@ -46,6 +48,33 @@ CREATE TABLE web_app.org_participants(
   participant_id INT REFERENCES web_app.pages(id) ON DELETE CASCADE,
   organisation_id INT REFERENCES web_app.pages(id) ON DELETE CASCADE,
   participation_type VARCHAR(10)
+);
+
+CREATE TABLE web_app.comments
+(
+  page_id INT,
+  post_id INT,
+  commentator_id INT,
+  comment_id INT,
+  content VARCHAR(4096),
+  time TIMESTAMP,
+  CONSTRAINT "post_id_fk" FOREIGN KEY (page_id, post_id) REFERENCES web_app.posts (page_id, post_id) ON DELETE CASCADE,
+  CONSTRAINT "page_initials" FOREIGN KEY (commentator_id) REFERENCES web_app.pages (id)  ON DELETE CASCADE
+);
+CREATE INDEX ON web_app.comments (page_id, post_id);
+
+CREATE TABLE web_app.subscribe(
+  subscriber_id INT,
+  source_id INT,
+  PRIMARY KEY (subscriber_id, source_id)
+
+);
+
+CREATE TABLE web_app.page_avatars(
+  page_id INT REFERENCES web_app.pages(id) ON DELETE CASCADE,
+  ava_id VARCHAR(20),
+  PRIMARY KEY (page_id)
+
 );
 
 GRANT USAGE ON SCHEMA web_app to tomcat;
